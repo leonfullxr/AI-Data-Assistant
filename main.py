3244e0bd19918f0a0a7e02f09a15ad3fe6f3550c
leon <l.elliottfuller@gmail.com>
2024-07-01 16:13:06 +0200

    Added the basic frontend

---
+++ main.py
@@ -0,0 +1,42 @@
+# import required libraries
+import os
+from apikey import api_key
+
+import streamlit as st
+import pandas as pd
+
+from langchain.llms import OpenAI
+from langchain_experimental.agents import create_pandas_dataframe_agent
+from dotenv import load_dotenv, find_dotenv
+
+# Main
+
+st.title('AI Assistant for Data Science')
+st.header('Welcome to the Ai Assistant for Data Science!')
+st.subheader('Solution')
+st.write('Hello, I am your AI Assistant and I am here to help you with your Data Science Projects.')
+
+
+with st.sidebar:
+    st.write('*Your Data Science Adventure begins with an CSV File.*')
+    st.caption('**Upload your CSV File and I will help you with the rest.**')
+    st.divider()
+    st.caption("<p style ='text-align:center'> Made by leonfullxr. </p>", unsafe_allow_html=True)
+    
+# Initialise the key in session state
+if 'clicked' not in st.session_state:
+    st.session_state.clicked = {1:False}
+    
+# Function to update the value in session state
+def clicked(button):
+    st.session_state.clicked[button] = True
+st.button("Let's get started", on_click=clicked, args=[1])
+if st.session_state.clicked[1]:
+    st.header('Exploratory Data Analysis Part')
+    st.subheader('Solution')
+    
+    user_csv = st.file_uploader("Upload your CSV file", type=['csv'])
+
+    if user_csv is not None:
+        user_csv.seek[0]
+        df = pd.read_csv(user_csv, low_memory=False)
